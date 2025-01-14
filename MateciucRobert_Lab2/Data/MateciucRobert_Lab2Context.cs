f9bd2086bc122082022e490e1752bb729c347ce0
User <User@ASUS>
2025-01-14 02:23:40 +0200

    Add project files.

---
+++ MateciucRobert_Lab2/Data/MateciucRobert_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MateciucRobert_Lab2.Models;
+
+namespace MateciucRobert_Lab2.Data
+{
+    public class MateciucRobert_Lab2Context : DbContext
+    {
+        public MateciucRobert_Lab2Context (DbContextOptions<MateciucRobert_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MateciucRobert_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<MateciucRobert_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ MateciucRobert_Lab2/Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MateciucRobert_Lab2.Data;
+using MateciucRobert_Lab2.Models;
+
+namespace MateciucRobert_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MateciucRobert_Lab2.Data.MateciucRobert_Lab2Context _context;
+
+        public IndexModel(MateciucRobert_Lab2.Data.MateciucRobert_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
